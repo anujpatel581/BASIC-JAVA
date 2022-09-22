02f81602948da171f615e76b52edcc2299f975a9
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-09-22 23:29:07 +0530

    TernaryOp
    TernaryOp

---
+++ OPERATOR/TernaryOp.java
@@ -0,0 +1,12 @@
+package Operator;
+
+public class TernaryOp { // ?
+    public static void main(String[] args){
+        int a=10;
+        int b=5;
+
+        int res=a>b ? a:b;
+        System.out.println(res);// aa>b is true then it print value of a , if a>b cond not true then it print value of b
+    }
+    
+}
