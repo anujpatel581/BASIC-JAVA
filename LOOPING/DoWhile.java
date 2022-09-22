9b04924e0ab9185b02b514d4e1688aa3073ef146
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-09-22 23:19:14 +0530

    Do while
    Do While

---
+++ LOOPING/DoWhile.java
@@ -0,0 +1,16 @@
+package Looping;
+
+public class DoWhile {
+    public static void main(String[] args){
+        int n = 1;
+        
+        do{
+            System.out.println(n);
+            n++;
+        }
+        while(n<=10);
+
+        
+    }
+    
+}
