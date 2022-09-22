0ada3385883ab868f0d61300e0efe4d103a38cc9
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-09-22 23:16:22 +0530

    Data types
    //int, short, long, float, double, char, string, byte

---
+++ DATA-TYPES/DataTypes.java
@@ -0,0 +1,24 @@
+public class DataTypes { //int, short, long, float, double, char, string, byte
+    public static void main(String[] args){
+        int a = 5;
+        long b = 123445556677777788l;
+        short c = 105;
+        float d = 66.677f;
+        double e = 55.56;
+        //double take default no need to write d at last digit of point value
+        char f = 'a';
+        String g = "hello";
+        byte h = 5;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+    }
+    
+}
