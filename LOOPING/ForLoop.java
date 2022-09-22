2af75772fd6bd18d0cfe5e146e8d6c32377e6e30
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-09-22 23:20:15 +0530

    ForLoop
    ForLoop

---
+++ LOOPING/ForLoop.java
@@ -0,0 +1,11 @@
+package Looping;
+
+public class ForLoop {
+    public static void main(String[] args){
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+            System.out.println(i+i);
+        }
+    }
+    
+}
