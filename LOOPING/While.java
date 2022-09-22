f7379d50e87eabfeaf5ff9bc9c8cb1d8a078fe08
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-09-22 23:22:27 +0530

    While Loop
    While Loop

---
+++ LOOPING/While.java
@@ -0,0 +1,12 @@
+package Looping;
+
+public class While {
+    public static void main(String[] args){
+        int n=1;
+        while(n<=10){
+            System.out.println(n);
+            n++;
+        }
+    }
+    
+}
