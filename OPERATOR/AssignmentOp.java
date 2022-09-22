9c9bc9e810952ced1a2d044aabf2e3a0ad16f45f
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-09-22 23:25:54 +0530

    AssignmentOp
    AssignmentOp

---
+++ OPERATOR/AssignmentOp.java
@@ -0,0 +1,15 @@
+package Operator;
+
+public class AssignmentOp {  //=, +=, -=, /=, *=, %=, (to assign the value)
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println(a+=b);
+        System.out.println(a-=b);
+        System.out.println(a/=b);
+        System.out.println(a*=b);
+        System.out.println(a%=b);
+        
+    }
+    
+}
