af89f7a47f3c82d6c3e651e5af11003fce627292
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-09-23 01:04:54 +0530

    Create Narrowing.java

---
+++ DATA-TYPES/Narrowing.java
@@ -0,0 +1,8 @@
+public class Narrowing{
+    public static void main(String[] args){
+        float mynum= 8.09f; // narrowing manually convert large data type to small dtata type size
+        int myint= (int) mynum; 
+        System.out.println(mynum);
+        System.out.println(myint);
+    }
+}
