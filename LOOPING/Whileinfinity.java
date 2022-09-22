d58643ad1b7ae67add02f2575f2909cbbe018279
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2022-09-22 23:23:41 +0530

    Whileinfinity loop
    Whileinfinity

---
+++ LOOPING/Whileinfinity.java
@@ -0,0 +1,11 @@
+package Looping;
+// umilimited
+public class Whileinfinity{
+    public static void main(String[] args){
+        int n = 1;
+        while(n>=0){
+            System.out.println("anuj");
+        }
+
+    }
+}
