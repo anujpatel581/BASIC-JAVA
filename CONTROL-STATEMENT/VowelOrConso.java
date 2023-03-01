de1f0ebc02d546a37d7bae890bbaa0fe673c179c
Anuj Patel <89893953+anujpatel581@users.noreply.github.com>
2023-03-01 14:58:26 +0530

    Create VowelOrConso.java

---
+++ CONTROL-STATEMENT/VowelOrConso.java
@@ -0,0 +1,6 @@
+if(ch=='a' || ch == 'e' || ch =='i' || ch=='o' || ch=='u'){
+              System.out.println("Vowel");
+              }
+              else{
+               System.out.println("Consonant");
+              }
